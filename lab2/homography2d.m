89cbce9c5217ece619581bd1d96b0e3d8bfe9dd3
Ivan Caminal <ivancaminal72@gmail.com>
2018-01-05 20:51:06 +0100

    last changes

--- lab2/homography2d.m
+++ lab2/homography2d.m
@@ -1,4 +1,7 @@
 function H = homography2d(x1, x2)
+    [x1, T1] = DLT_normalization(x1);
+    [x2, T2] = DLT_normalization(x2);
+    
     A = zeros(8,9);
     Ai = zeros(2,9);
     
@@ -11,5 +14,22 @@ function H = homography2d(x1, x2)
         A(i:i+1,:) = Ai;
     end
     [~,~,V] = svd(A);
-    H=reshape(V(:,size(V,2)),[3,3]);
+    H=inv(T2')*reshape(V(:,size(V,2)),[3,3])*T1;
+end
+
+function [ x_normalized, T ] = DLT_normalization(x)
+    x(1,1:4)=x(1,1:4)./x(3,1:4);
+    x(2,1:4)=x(2,1:4)./x(3,1:4);
+    x(3,1:4)=1;
+    
+    x_mean = mean(x(1:2,1:4));
+    x_new(1,1:4) =  x(1,1:4)-x_mean(1);
+    x_new(2,1:4) = x(2,1:4)-x_mean(2);
+    dist = sqrt(x_new(1,1:4).^2 + x_new(2,1:4).^2);
+    meandist = mean(dist(:).^2);  
+    s = sqrt(2)/meandist;
+    T = [s   0   -s*x_mean(1)
+         0   s   -s*x_mean(2)
+         0   0      1      ];
+    x_normalized = T*x;
 end
