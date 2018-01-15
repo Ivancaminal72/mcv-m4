7a15ee5bb0ecb05c0c6f6166a0c3ac27888b1c6a
Ivan Caminal <ivancaminal72@gmail.com>
2018-01-15 21:09:13 +0100

    funcion

---
+++ lab3/fundamental_matrix.m
@@ -0,0 +1,14 @@
+function F_es = fundamental_matrix(x1_test, x2_test)
+    p1 = x1_test(3,:);
+    p2 = x2_test(3,:);
+    
+    W = [(p1(1,:).*p2(1,:))', (p1(1,:).*p2(2,:))', (p1(1,:))', ...
+         (p1(2,:).*p2(1,:))', (p1(2,:).*p2(2,:))', (p1(2,:))', ...
+         (p2(1,:))'         , (p2(2,:))'         , ones(8,1)];
+    
+    [~,~,V] = svd(W);
+    F = reshape(V(:,size(V,2)),[3,3]);
+    [U,D,V] = svd(F);
+    D(3,3)=0;
+    F_es = U*D*V';
+end
